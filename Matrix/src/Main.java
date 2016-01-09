b545a2c30f9e6cf8b893abb06f10b282361dc801
krafft12 <andreikrafft@gmail.com>
2016-01-09 18:04:58 +0200

    PatratPerfect etc

--- Matrix/src/Main.java
+++ Matrix/src/Main.java
@@ -1,6 +1,7 @@
 
 
 import java.io.FileNotFoundException;
+
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
@@ -13,9 +14,33 @@ public class Main {
 		WorkMatrix workSquare = new WorkMatrix(readMatrix.getMatrix());
 
 		PrintWriter writer = new PrintWriter("src/out.txt", "UTF-8");
-		
+		int[][] matrix = readMatrix.getMatrix();
+		System.out.println("ok");
 		writer.print(readMatrix.toString());
 		writer.println("Suma matrice= "+workSquare.SumaMatrix());
-		writer.close();
+		writer.println("Pare sunt="+workSquare.Pare());
+		writer.println("Impare sunt="+workSquare.imPare());
+		writer.print("Numerele pare : ");
+		for (int i = 0; i < workSquare.AfisPare(matrix).length; i++) 
+		{
+			writer.print(workSquare.AfisPare(matrix)[i] + " ");
+		}
+		writer.println();
+		writer.print("Numere impare : ");
+		for (int i = 0; i < workSquare.AfisimPare(matrix).length; i++) 
+		{
+			writer.print(workSquare.AfisimPare(matrix)[i] + " ");
+		}
+		writer.println();
+		writer.println("Minim este="+workSquare.minim());
+		writer.println("Maxim este="+workSquare.maxim());
+		writer.println("Patrate perfecte sunt="+workSquare.PatratPerfect());
+		writer.print("Elemente patrate perfecte sunt= : ");
+		for (int i = 0; i < workSquare.AfisareP(matrix).length; i++) 
+		{
+			writer.print(workSquare.AfisareP(matrix)[i] + " ");
+		}
+		writer.println();
+;		writer.close();
 	}
 }
--- Matrix/src/WorkMatrix.java
+++ Matrix/src/WorkMatrix.java
@@ -1,5 +1,6 @@
 
-
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class WorkMatrix {
 	private int[][] matrix;
@@ -15,6 +16,14 @@ public class WorkMatrix {
 	public void setMatrix(int[][] matrix) {
 		this.matrix = matrix;
 	}
+		
+			int[] addElement(int[] org, int added) {
+				int[] result = Arrays.copyOf(org, org.length + 1);
+				result[org.length] = added;
+				return result;
+		
+		
+	}
 	public int SumaMatrix(){
 		int s = 0;
 		for (int i = 0; i < matrix.length; i++) {
@@ -25,25 +34,110 @@ public class WorkMatrix {
 		return s;
 	}
 	public int Pare(){
-		int s = 0;
+		int par = 0;
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix.length; j++) {
-				if matrix[i][j]%2=0 
+				if (matrix[i][j]%2==0 )
 						par=par+1;
+				}
+			}
+		
+		return par;
+}
+	public int imPare(){
+		int impar = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				if (matrix[i][j]%2!=0 )
+						impar=impar+1;
 				
 			}
 		}
-		return par;
-	}
+		return impar;
+}
+	
 
-public int imPare(){
-	int impar = 0;
-	for (int i = 0; i < matrix.length; i++) {
-		for (int j = 0; j < matrix.length; j++) {
-			if (matrix[i][j]%2!=0 )
-					impar=impar+1;
-			
+
+	public int [] AfisPare(int[][] matrix)
+	{
+		int temp[] = new int[0] ;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				if (matrix[i][j]%2==0 ) {
+					temp = addElement(temp, matrix[i][j]);
+				
+}
+		
+			}
+		}
+		return temp; 
+		
+	}
+	public int [] AfisimPare(int[][] matrix)
+	{
+		int temp[] = new int[0] ;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				if (matrix[i][j]%2==1 ) {
+					temp = addElement(temp, matrix[i][j]);
+				
+}
+		
+			}
+		}
+		return temp; 
+		
+	}
+	public int minim(){
+		int min=10;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				if (matrix[i][j]<min)
+					min=matrix[i][j];
+			}
+		}
+		return min;
+	}
+		public int maxim(){
+			int max=0;
+					for (int i = 0; i < matrix.length; i++) {
+						for (int j = 0; j < matrix.length; j++) {
+							if (matrix[i][j]>max)
+								max=matrix[i][j];
+						}
+						}
+					return max;
+		}
+		public double PatratPerfect(){
+			double p;
+			int s=0;
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix.length; j++) {
+					p=Math.sqrt(matrix[i][j]);
+					if (matrix[i][j]/p==p)
+						s++;
+				}
+		}
+			return s;
+		} 
+
+
+		public int[] AfisareP(int[][] matrix) {
+			double p;
+			int[] temp = new int[0];
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix.length; j++) {
+					p = Math.sqrt(matrix[i][j]);
+					if (p*p==matrix[i][j]) {
+						temp = addElement(temp, matrix[i][j]);
 		}
 	}
-	return impar;
-}
+			}
+			return temp;
+		}
+		}
+		
+		
+	
+		
+	
